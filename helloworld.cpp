8843c7d6369aad3a94d5b7d4cf0b2191f109d92b
Pranshu Bhardwaj <pran@uw.edu>
2021-10-03 15:41:17 -0700

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+/*
+*
+* @author Pranshu Bhardwaj
+* @date 2021-10-3
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void test();
+
+int main(){
+    cout << "Hello World" << endl;
+    test();
+    return 0;
+}
---
+++ utilities.cpp
@@ -0,0 +1,15 @@
+/*
+*
+* @author Pranshu Bhardwaj
+* @date 2021-10-3
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//This is for testing multifile compiling and running
+void test(){
+    cout << "Test succesfull" << endl; 
+}
